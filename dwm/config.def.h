1a98bdb6fcbcdbc4895817a98d54f790c6feaf34
coss <i60888@163.com>
2022-02-06 21:48:20 +0800

    tags

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,7 +1,7 @@
 /* ASee LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 36;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -32,8 +32,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-/*static const char *tags[] = {  " ", " ", " ", " " };*/
-static const char *tags[] = {  "1", "2", "3", "4", "5" };
+static const char *tags[] = { "", "", "", "" };
+/* static const char *tags[] = {  "1", "2", "3", "4", "5" }; */
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -73,7 +73,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
 
-static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *stcmd[]  = { "st", NULL };
 static const char *flameshot[]  = { "flameshot","gui", NULL };
